e28819d8c2d549d751cbd51dad548cc61484958b
gurzilo <joaogurzilo@gmail.com>
2025-05-15 22:55:57 -0300

    instalacoes e organizacoes de arquivos concluidos
    add setup
    add __main__

---
+++ dundie/__init__.py
@@ -0,0 +1,1 @@
+print("INICIALIZANDO DUNDIE...")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundiegur",
+    version="0.1.0",
+    description="Recompensa de pontos, bonus para funcionarios da empresa.",
+    author="João Gurzilo",
+    packages=find_packages(),
+)
